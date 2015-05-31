9b3a913a793ce1590257e935d87e567604d785bf
Nick Webb <nickw@redwireservices.com>
2015-05-31 16:03:32 -0700

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,8 @@
 default['bamboo-agent']['server']['address'] = 'localhost'
 default['bamboo-agent']['server']['protocol'] = 'http'
 default['bamboo-agent']['server']['port'] = 8085
-default['bamboo-agent']['server']['url'] = "#{node['bamboo-agent']['server']['protocol']}://#{node['bamboo-agent']['server']['address']}:#{node['bamboo-agent']['server']['port']}"
+default['bamboo-agent']['server']['url_prefix'] = 'bamboo'
+default['bamboo-agent']['server']['url'] = "#{node['bamboo-agent']['server']['protocol']}://#{node['bamboo-agent']['server']['address']}:#{node['bamboo-agent']['server']['port']}/#{node['bamboo-agent']['server']['url_prefix']}"
 
 default['bamboo-agent']['install_dir'] = '/usr/local/bamboo'
 default['bamboo-agent']['installer_jar'] = "#{node['bamboo-agent']['install_dir']}/bamboo-agent-installer.jar"
